a5a12cdee671c1149771bfb9671ae5b01266aed3
Safura234 <safuramg@code.edu.az>
2022-04-10 16:18:07 +0400

    https://github.com/Safura234/angle.git

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle");
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
